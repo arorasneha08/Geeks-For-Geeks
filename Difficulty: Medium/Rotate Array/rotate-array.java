08a61c3b18472f7055194b78802723e6f42aef83
arorasneha08 <158125379+arorasneha08@users.noreply.github.com>
2025-01-14 17:43:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate Array/rotate-array.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            String line = in.readLine();
+            String[] tokens = line.split(" ");
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int d = Integer.parseInt(in.readLine().trim()); // rotation count (key)
+            int n = array.size();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = array.get(i);
+
+            new Solution().rotateArr(arr, d); // rotating the array
+            StringBuilder sb = new StringBuilder();
+
+            // printing the elements of the array
+            for (int value : arr) sb.append(value).append(" ");
+            out.println(sb.toString().trim());
+
+            out.println("~");
+        }
+        out.flush();
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static void rotateArr(int arr[], int d) {
+        int n= arr.length ;
+        d = d % n ; 
+        int k = 0 ;
+        int a[] = new int[n];
+        for(int i = d ; i<n ;i++){
+            a[k++] = arr[i]; 
+        }
+        for(int i = 0 ; i<d ; i++){
+            a[k++] = arr[i]; 
+        }
+        for(int i = 0; i<n ; i++){
+            arr[i] = a[i]; 
+        }
+    }
+}
