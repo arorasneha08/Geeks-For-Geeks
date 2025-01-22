377c7fa82452031efeebb41bfa53274799e1fcc3
arorasneha08 <158125379+arorasneha08@users.noreply.github.com>
2025-01-22 12:34:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Max rectangle/max-rectangle.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class FindMinCost {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int arr[][] = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+            System.out.println(new Solution().maxArea(arr));
+            t--;
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*Complete the function given below*/
+class Solution {
+    static int largestRectangleHist(int arr[], int n){
+        Stack<Integer> st = new Stack<>();
+        int area = 0 ; 
+        for(int i = 0 ; i<n ; i++){
+            while(!st.isEmpty() && arr[st.peek()] > arr[i]){
+                int h = arr[st.pop()]; 
+                int w = st.empty() ? i : i - st.peek()-1 ; 
+                area = Math.max(area , h * w) ; 
+            }
+            st.push(i); 
+        }
+        while(!st.isEmpty()){
+            int h = arr[st.pop()] ; 
+            int w = st.empty() ? n : n-st.peek()-1 ; 
+            area = Math.max(area , h*w ); 
+        }
+        return area ; 
+    }
+    static int maxArea(int mat[][]) {
+        int n = mat.length ;
+        int m = mat[0].length ; 
+        int prefSum[][] = new int[n][m];
+        for(int j = 0 ; j< m ; j++){
+            for(int i = 0 ; i<n ; i++){
+                if(mat[i][j] == 0){
+                    prefSum[i][j] = 0 ; 
+                }
+                else{
+                    prefSum[i][j] = (i == 0) ?  1 : prefSum[i-1][j] + 1 ; 
+                }
+            }
+        }
+        int maxArea = 0 ;
+        for(int i = 0 ; i<n ; i++){
+            maxArea = Math.max(maxArea , largestRectangleHist(prefSum[i] , m)); 
+        }
+        return maxArea ; 
+    }
+}
