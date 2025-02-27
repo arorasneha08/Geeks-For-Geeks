fd50f77b7902bfdee75b4daf0e5dd2e067513aea
arorasneha08 <158125379+arorasneha08@users.noreply.github.com>
2025-02-27 11:04:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Get Min from Stack/get-min-from-stack.java
+++ Difficulty: Medium/Get Min from Stack/get-min-from-stack.java
@@ -1,94 +1,84 @@
 //{ Driver Code Starts
 import java.util.*;
 
+class Get_Min_From_Stack {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); // Number of test cases
 
+        while (T-- > 0) {
+            int q = sc.nextInt(); // Number of queries
+            Solution g = new Solution();
 
-class Get_Min_From_Stack
-{
-	public static void main(String args[])
-	{
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		while(T>0)
-		{
-			int q = sc.nextInt();
-			GfG g = new GfG();
-			while(q>0)
-			{
-				int qt = sc.nextInt();
-				
-				if(qt == 1)
-				{
-					int att = sc.nextInt();
-					g.push(att);
-					//System.out.println(att);
-				}
-				else if(qt == 2)
-				{
-					System.out.print(g.pop()+" ");
-				}
-				else if(qt == 3)
-				{
-					System.out.print(g.getMin()+" ");
-				}
-			
-			q--;
-			}
-			System.out.println();
-		T--;
-		
-System.out.println("~");
+            while (q-- > 0) {
+                int qt = sc.nextInt();
+
+                if (qt == 1) {
+                    int att = sc.nextInt();
+                    g.push(att);
+                } else if (qt == 2) {
+                    g.pop(); // Just call pop(), do not print anything
+                } else if (qt == 3) {
+                    System.out.print(g.peek() + " "); // Print top element
+                } else if (qt == 4) {
+                    System.out.print(g.getMin() + " "); // Print minimum element
+                }
+            }
+            System.out.println("\n~");
+        }
+        sc.close();
+    }
 }
-		
-	}
-}
-
-
 
 // } Driver Code Ends
 
 
-class GfG {
-    int minElem; // Stores the current minimum element
-    Stack<Integer> st; // Main stack
+class Solution {
+    Stack<Integer> st = new Stack<>(); 
+    int minElem;  
 
-    GfG() {
-        minElem = 0;
-        st = new Stack<>();
+    public Solution() {
+        minElem = -1; // Default value for an empty stack
     }
 
-    // Get the minimum element in the stack
-    int getMin() {
-        if (st.isEmpty()) return -1; // Return -1 if the stack is empty
-        return minElem;
-    }
-
-    // Pop an element from the stack
-    int pop() {
-        if (st.isEmpty()) return -1; // Return -1 if the stack is empty
-        int top = st.pop();
-
-        // If the popped element is less than minElem, it means minElem was stored in encoded form
-        if (top < minElem) {
-            int originalMin = minElem; // Retrieve the original minimum
-            minElem = 2 * minElem - top; // Update minElem to the previous minimum
-            return originalMin;
-        }
-        return top; // Otherwise, return the normal top value
-    }
-
-    // Push an element into the stack
-    void push(int x) {
+    // Add an element to the top of Stack
+    public void push(int x) {
         if (st.isEmpty()) {
             st.push(x);
-            minElem = x; // Initialize minElem for the first element
+            minElem = x;
         } else if (x < minElem) {
-            st.push(2 * x - minElem); // Encode the new minimum element
-            minElem = x; // Update minElem
+            st.push(2 * x - minElem); // Store the encoded value
+            minElem = x;  
         } else {
-            st.push(x); // Push normally if x is greater than or equal to minElem
+            st.push(x);
         }
     }
+
+    // Remove the top element from the Stack
+    public void pop() {
+        if (st.isEmpty()) return; 
+
+        int top = st.pop();
+        if (top < minElem) { // If top is an encoded value
+            minElem = 2 * minElem - top; 
+        }
+
+        // If stack becomes empty after popping, reset minElem
+        if (st.isEmpty()) {
+            minElem = -1;
+        }
+    }
+
+    // Returns top element of the Stack
+    public int peek() {
+        if (st.isEmpty()) return -1; // Return -1 for empty stack
+
+        int top = st.peek();
+        return (top < minElem) ? minElem : top;
+    }
+
+    // Finds minimum element of Stack
+    public int getMin() {
+        return (st.isEmpty()) ? -1 : minElem;
+    }
 }
-
-
