f0c4fc1bfefc79ef0f783fee8640105079e1262d
arorasneha08 <158125379+arorasneha08@users.noreply.github.com>
2025-02-27 17:13:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Introduction to DP/introduction-to-dp.java
+++ Difficulty: Medium/Introduction to DP/introduction-to-dp.java
@@ -32,7 +32,7 @@ System.out.println("~");
 
 class Solution {
     static long fibonacci(int n , long[] dp){
-        if(n <= 1) return n ; 
+        if(n == 0 || n == 1) return n ; 
         if(dp[n] != -1) return dp[n] ; 
         dp[n] = (fibonacci(n-1 , dp) + fibonacci(n-2 , dp)) % 1000000007 ; 
         return dp[n]; 
@@ -45,13 +45,13 @@ class Solution {
     }
 
     static long bottomUp(int n) {
-        if(n <= 0) return n ; 
+        if(n == 1 || n == 0) return n ; 
         long dp[] = new long[n+1];
         dp[0] = 0 ; 
-        dp[1] = 1;  
+        dp[1] = 1 ;  
         for(int i = 2 ; i<= n ; i++){
             dp[i] = (dp[i-1] + dp[i-2] ) % 1000000007; 
-        }
+        }   
         return dp[n]; 
     }
 }
