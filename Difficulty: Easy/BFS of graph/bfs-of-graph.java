e359224d2905dd8441a0e60c8860af084244b03f
arorasneha08 <158125379+arorasneha08@users.noreply.github.com>
2025-04-02 00:37:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -1,53 +1,17 @@
 //{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
 import java.util.*;
 
-// Driver code
-class GFG {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int testcases = sc.nextInt(); // Taking number of test cases as input
-
-        while (testcases-- > 0) {
-            int V = sc.nextInt(); // Number of vertices
-            int E = sc.nextInt(); // Number of edges
-
-            // Initialize adjacency list
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-            for (int i = 0; i < V; i++) {
-                adj.add(new ArrayList<>()); // Create a new list for each vertex
-            }
-
-            // Add edges to the adjacency list
-            for (int i = 0; i < E; i++) {
-                int u = sc.nextInt();
-                int v = sc.nextInt();
-                adj.get(u).add(v); // Adding edge u -> v
-                adj.get(v).add(u); // Adding edge v -> u (undirected graph)
-            }
-
-            // Create Solution object and call bfsOfGraph
-            Solution obj = new Solution();
-            ArrayList<Integer> result = obj.bfsOfGraph(V, adj);
-
-            // Print the result
-            for (int node : result) {
-                System.out.print(node + " ");
-            }
-            System.out.println();
-        }
-
-        sc.close(); // Close the scanner
-    }
-}
 
 // } Driver Code Ends
 
-
 // User function Template for Java
 class Solution {
-    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+    // Function to return Breadth First Search Traversal of given graph.
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
         Queue<Integer> q = new LinkedList<>(); 
-        boolean visited[] = new boolean[V]; 
+        boolean visited[] = new boolean[adj.size()]; 
         ArrayList<Integer> bfs = new ArrayList<>(); 
         q.add(0);
         visited[0] = true ;
@@ -65,3 +29,39 @@ class Solution {
         return bfs ; 
     }
 }
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+            int V = Integer.parseInt(br.readLine().trim());
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+            for (int i = 0; i < V; i++) {
+                String[] input = br.readLine().trim().split(" ");
+                ArrayList<Integer> node = new ArrayList<>();
+                for (String s : input) {
+                    if (!s.isEmpty()) {
+                        node.add(Integer.parseInt(s));
+                    }
+                }
+                adj.add(node);
+            }
+
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.bfs(adj);
+            for (int num : ans) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
