23c0cc3ba305080c3d43bc9690485f74cb034aff
arorasneha08 <158125379+arorasneha08@users.noreply.github.com>
2025-04-11 09:39:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
@@ -1,102 +1,95 @@
 //{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
 import java.util.*;
 
-class iPair {
-    int first, second;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    iPair(int first, int second) {
-        this.first = first;
-        this.second = second;
-    }
-}
+        int t = Integer.parseInt(sc.nextLine());
 
-class DriverClass {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
         while (t-- > 0) {
-            String str[] = read.readLine().trim().split(" ");
-            int V = Integer.parseInt(str[0]);
-            int E = Integer.parseInt(str[1]);
+            int V = Integer.parseInt(sc.nextLine());
+            int E = Integer.parseInt(sc.nextLine());
 
-            ArrayList<ArrayList<iPair>> adj = new ArrayList<ArrayList<iPair>>();
-            for (int i = 0; i < V; i++) {
-                adj.add(new ArrayList<iPair>());
+            List<int[]> edgeList = new ArrayList<>();
+
+            for (int i = 0; i < E; i++) {
+                String[] parts = sc.nextLine().split(" ");
+                int u = Integer.parseInt(parts[0]);
+                int v = Integer.parseInt(parts[1]);
+                int w = Integer.parseInt(parts[2]);
+                edgeList.add(new int[] {u, v, w});
+                edgeList.add(new int[] {v, u, w});
             }
 
-            int i = 0;
-            while (i++ < E) {
-                String S[] = read.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                int w = Integer.parseInt(S[2]);
-                iPair t1 = new iPair(v, w);
-                iPair t2 = new iPair(u, w);
-                adj.get(u).add(t1);
-                adj.get(v).add(t2);
+            int[][] edges = new int[edgeList.size()][3];
+            for (int i = 0; i < edgeList.size(); i++) {
+                edges[i] = edgeList.get(i);
             }
 
-            int src = Integer.parseInt(read.readLine());
+            int src = Integer.parseInt(sc.nextLine());
 
-            Solution ob = new Solution();
+            Solution obj = new Solution();
+            int[] res = obj.dijkstra(V, edges, src);
 
-            ArrayList<Integer> res = ob.dijkstra(adj, src);
-
-            for (i = 0; i < V; i++) System.out.print(res.get(i) + " ");
+            for (int val : res) {
+                System.out.print(val + " ");
+            }
             System.out.println();
-
             System.out.println("~");
         }
+
+        sc.close();
     }
 }
+
 // } Driver Code Ends
 
 
-/*
-class iPair {
-    int first, second;
-
-    iPair(int first, int second) {
-        this.first = first;
-        this.second = second;
-    }
-}
-*/
-
 // User function Template for Java
 class Solution {
-    ArrayList<Integer> dijkstra(ArrayList<ArrayList<iPair>> adj, int src) {
-        PriorityQueue<iPair> pq = new PriorityQueue<>((a,b)-> a.second - b.second);
-        int n = adj.size(); 
-        ArrayList<Integer> distance = new ArrayList<>() ;
-        for(int i = 0 ; i<n ; i++){
-            distance.add((int)1e9); 
+    public class Pair{
+        int node ;
+        int dist ; 
+        Pair(int node , int dist){
+            this.node = node;
+            this.dist = dist; 
         }
-        distance.set(src, 0); 
-        pq.add(new iPair(src, 0));
+    }
+    public int[] dijkstra(int V, int[][] edges, int src) {
+        List<List<Pair>> adj = new ArrayList<>(); 
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.dist - b.dist); 
+        int distance[] = new int[V];
+        
+        for(int i = 0 ; i < V ; i++){
+            adj.add(new ArrayList<>()); 
+        }
+        for(int edge[] : edges){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2]; 
+            adj.get(u).add(new Pair(v, w)); 
+        }
+        Arrays.fill(distance , Integer.MAX_VALUE); 
+        distance[src] = 0 ;
+        pq.offer(new Pair(src , 0)); 
         
         while(!pq.isEmpty()){
-            iPair curr = pq.poll();
-            int node = curr.first ;
-            int dist = curr.second;
+            Pair curr = pq.poll();
+            int node = curr.node;
+            int dist = curr.dist; 
             
-            for(iPair neighbour : adj.get(node)){
-                int adjNode = neighbour.first ;
-                int weight = neighbour.second;
+            for(Pair neighbour : adj.get(node)){
+                int adjNode = neighbour.node ;
+                int weight = neighbour.dist ;
                 
-                if(dist + weight < distance.get(adjNode)){
-                    distance.set(adjNode, dist + weight);
-                    pq.add(new iPair(adjNode, dist + weight));
+                if(dist + weight < distance[adjNode]){
+                    distance[adjNode] = dist + weight ; 
+                    pq.offer(new Pair(adjNode , distance[adjNode])); 
                 }
             }
         }
-        for (int i = 0; i < n; i++) {
-            if (distance.get(i) == (int) 1e9) {
-                distance.set(i, -1);
-            }
-        }
+        
         return distance; 
     }
 }
