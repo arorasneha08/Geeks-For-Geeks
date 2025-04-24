e9200956824b60b242629bbe7c9027fa3492329c
arorasneha08 <158125379+arorasneha08@users.noreply.github.com>
2025-04-24 09:44:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Unique Number III/unique-number-iii.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            String[] arr1Str = sc.nextLine().split(" ");
+            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
+            Solution ob = new Solution();
+            int ans = ob.getSingle(arr);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int getSingle(int[] arr) {
+        Arrays.sort(arr);
+        int n = arr.length ; 
+        for(int i = 1 ; i < n ; i+=3){
+            if(arr[i-1] != arr[i]){
+                return arr[i-1]; 
+            }
+        }
+        return arr[n-1]; 
+    }
+}
