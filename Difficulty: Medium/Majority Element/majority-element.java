04cfce6a5fb61d0e5cc478d2e583830afbbd821c
arorasneha08 <158125379+arorasneha08@users.noreply.github.com>
2025-04-25 08:51:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element/majority-element.java
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -14,10 +14,13 @@ class Geeks {
             int arr[] = new int[str.length];
             for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
             System.out.println(new Solution().majorityElement(arr));
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
+
+
 
 
 // User function Template for Java
@@ -26,28 +29,28 @@ class Solution {
     static int majorityElement(int arr[]) {
         int elem = -1 ;
         int n = arr.length ;
-    int count = 0 ; 
-    for(int i = 0 ; i<n ; i++){
-        if(count == 0){
-            count = 1 ; 
-            elem = arr[i] ;
+        int count = 0 ; 
+        for(int i = 0 ; i<n ; i++){
+            if(count == 0){
+                count = 1 ; 
+                elem = arr[i] ;
+            }
+            else if(elem == arr[i]){
+                count ++ ; 
+            }
+            else{
+                count --; 
+            }
         }
-        else if(elem == arr[i]){
-            count ++ ; 
+        int count1 = 0 ;
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] == elem){
+                count1 ++ ;
+            }
         }
-        else{
-            count --; 
+        if(count1 > n/2){
+            return elem ;
         }
-    }
-    int count1 = 0 ;
-    for(int i = 0 ; i < n ; i++){
-        if(arr[i] == elem){
-            count1 ++ ;
-        }
-    }
-    if(count1 > n/2){
-        return elem ;
-    }
-    return -1 ;
+        return -1 ;
     }
 }
