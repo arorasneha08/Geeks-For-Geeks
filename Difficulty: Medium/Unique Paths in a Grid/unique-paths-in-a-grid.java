6642181155b46c58ba30f8468e3f988bc212b499
arorasneha08 <158125379+arorasneha08@users.noreply.github.com>
2025-06-02 15:27:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Unique Paths in a Grid/unique-paths-in-a-grid.java
+++ Difficulty: Medium/Unique Paths in a Grid/unique-paths-in-a-grid.java
@@ -1,57 +1,27 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String S[] = read.readLine().split(" ");
-            
-            int n = Integer.parseInt(S[0]);
-            int m = Integer.parseInt(S[1]);
-            
-            int [][] grid = new int[n][m];
-            for(int i=0; i<n; i++)
-            {
-                String S1[] = read.readLine().split(" ");
-                for(int j=0; j<m; j++)
-                {
-                    grid[i][j] = Integer.parseInt(S1[j]);
+class Solution {
+    public int uniquePaths(int[][] grid) {
+        int rowlen = grid.length;
+        int columnlen = grid[0].length;
+        if (grid[0][0] == 1 || grid[rowlen - 1][columnlen - 1] == 1) {
+            return 0;
+        }
+        int[][] dp = new int[rowlen][columnlen];
+        dp[0][0] = 1;
+        for (int i = 0; i < rowlen; i++) {
+            for (int j = 0; j < columnlen; j++) {
+                if (grid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    if (i > 0) {
+                        dp[i][j] += dp[i - 1][j];
+                    }
+                    if (j > 0) {
+                        dp[i][j] += dp[i][j - 1];
+                    }
                 }
             }
+        }
 
-            Solution ob = new Solution();
-            System.out.println(ob.uniquePaths(n,m,grid));
-        
-System.out.println("~");
-}
+        return dp[rowlen - 1][columnlen - 1];
     }
 }
-// } Driver Code Ends
-
-
-class Solution {
-    static int mod = (int) 1e9 + 7 ; 
-    
-    public static int func(int i , int j , int dp[][] , int grid[][]){
-        if(i < 0 || j < 0 || grid[i][j] == 0) return 0 ; 
-        if(i == 0 && j == 0){
-            return 1 ; 
-        }
-        if(dp[i][j] != -1) return dp[i][j] ; 
-        int up = func(i-1 , j , dp , grid);
-        int left = func(i , j-1 , dp , grid);
-        return dp[i][j] = (up + left ) % mod; 
-    }
-    static int uniquePaths(int n, int m, int[][] grid) {
-        if(grid[0][0] == 0|| grid[n-1][m-1] == 0) return 0 ; 
-        int dp[][] = new int[n][m];
-        for(int row[] : dp){
-            Arrays.fill(row, -1); 
-        }
-        return func(n-1 , m-1 , dp , grid); 
-    }
-};
