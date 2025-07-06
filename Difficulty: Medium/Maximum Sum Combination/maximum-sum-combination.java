4543e9f04251e7ab1308a3803d6c6df25854d6bb
arorasneha08 <158125379+arorasneha08@users.noreply.github.com>
2025-07-06 14:23:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Sum Combination/maximum-sum-combination.java
+++ Difficulty: Medium/Maximum Sum Combination/maximum-sum-combination.java
@@ -1,45 +1,11 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.util.*;
-import java.lang.*;
-import java.math.*;
-import java.io.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (T-- > 0) {
-            int n = sc.nextInt();
-            int k = sc.nextInt();
-            int a[] = new int[n];
-            for (int i = 0; i < n; i++) {
-                a[i] = sc.nextInt();
-            }
-            int b[] = new int[n];
-            for (int i = 0; i < n; i++) {
-                b[i] = sc.nextInt();
-            }
-
-            Solution obj = new Solution();
-            List<Integer> ans = obj.maxCombinations(n, k, a, b);
-            for (int e : ans) System.out.print(e + " ");
-            System.out.println();
-        
-System.out.println("~");
-}
-    }
-}
-
-// } Driver Code Ends
 
 
 // User function Template for Java
 
 class Solution {
-    static List<Integer> maxCombinations(int n, int k, int a[], int b[]) {
-        List<Integer> res = new ArrayList<>();
+    static ArrayList<Integer> topKSumPairs(int a[], int b[], int k) {
+        int n = a.length ; 
+        ArrayList<Integer> res = new ArrayList<>();
         
         Arrays.sort(a);
         Arrays.sort(b);
@@ -58,19 +24,16 @@ class Solution {
 
             int i = top[1], j = top[2];
 
-            // Try adding the next largest pair in the row (i-1, j)
             if (i - 1 >= 0 && !visited.contains((i - 1) + "," + j)) {
                 pq.offer(new int[]{a[i - 1] + b[j], i - 1, j});
                 visited.add((i - 1) + "," + j);
             }
 
-            // Try adding the next largest pair in the column (i, j-1)
             if (j - 1 >= 0 && !visited.contains(i + "," + (j - 1))) {
                 pq.offer(new int[]{a[i] + b[j - 1], i, j - 1});
                 visited.add(i + "," + (j - 1));
             }
         }
-
         return res;
     } 
 }
