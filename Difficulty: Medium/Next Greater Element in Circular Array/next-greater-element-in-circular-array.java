4b2053488c6870c56fc5c3dedd9f0284b9274b47
arorasneha08 <158125379+arorasneha08@users.noreply.github.com>
2025-07-07 10:13:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element in Circular Array/next-greater-element-in-circular-array.java
+++ Difficulty: Medium/Next Greater Element in Circular Array/next-greater-element-in-circular-array.java
@@ -1,45 +1,12 @@
-//{ Driver Code Starts
-/*package whatever //do not write package name here */
-
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-class GFG {
-    
-	public static void main (String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter ot = new PrintWriter(System.out);
-		int t = Integer.parseInt(br.readLine().trim());
-		while(t-->0){
-		    int n = Integer.parseInt(br.readLine().trim());
-		    String inputLine[] = br.readLine().trim().split(" ");
-		    long[] arr = new long[n];
-		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
-		    Solution ob = new Solution();
-		    long[] res = ob.nextLargerElement(arr, n);
-		    for (int i = 0; i < n; i++) 
-		        ot.print(res[i] + " ");
-		    ot.println();
-		}
-        ot.close();
-	}
-}
-
-
-
-// } Driver Code Ends
-
-
-// User Function Template for JAVA
-
-class Solution{
-    public static long[] nextLargerElement(long[] arr, int n) { 
-        Stack<Long> st = new Stack<>();
-        long res[] = new long[n]; 
+class Solution {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        int n = arr.length; 
+        int ans[] = new int[n]; 
+        ArrayList<Integer> res = new ArrayList<>(); 
         
-        for(int i = 0 ; i<n ; i++){
-            res[i] = -1; 
+        for(int i = 0 ; i < n ; i++){
+            ans[i] = -1; 
         }
         
         for(int i = 2 * n - 1 ; i >= 0 ; i--){
@@ -47,10 +14,13 @@ class Solution{
                 st.pop(); 
             }
             if(!st.isEmpty()){
-                res[i% n] = st.peek(); 
+                ans[i% n] = st.peek(); 
             }
             st.push(arr[i % n]); 
         }
+        for(int i = 0 ; i < n ; i++){
+            res.add(ans[i]);
+        }
         return res ; 
-    } 
+    }
 }
