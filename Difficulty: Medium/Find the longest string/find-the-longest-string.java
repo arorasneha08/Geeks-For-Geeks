c50c3a9bcd4e26cb7e7bafbd09e50a83fe30c12d
arorasneha08 <158125379+arorasneha08@users.noreply.github.com>
2025-07-10 14:13:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the longest string/find-the-longest-string.java
+++ Difficulty: Medium/Find the longest string/find-the-longest-string.java
@@ -1,56 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-
-class StringArray
-{
-    public static String[] input(BufferedReader br, int n) throws IOException
-    {
-        String[] s = br.readLine().trim().split(" ");
-        return s;
-    }
-
-    public static void print(String[] a)
-    {
-        for(String e : a)
-            System.out.print(e + " ");
-        System.out.println();
-    }
-
-    public static void print(ArrayList<String> a)
-    {
-        for(String e : a)
-            System.out.print(e + " ");
-        System.out.println();
-    }
-}
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t;
-        t = Integer.parseInt(br.readLine());
-        while(t-- > 0){
-            
-            int n;
-            n = Integer.parseInt(br.readLine());
-            
-            
-            String[] arr = StringArray.input(br, n);
-            
-            Solution obj = new Solution();
-            String res = obj.longestString(n, arr);
-            
-            System.out.println(res);
-            
-        
-System.out.println("~");
-}
-    }
-}
-
-// } Driver Code Ends
 
 
 class Solution {
@@ -113,8 +60,9 @@ class Solution {
             return true ; 
         }
     }
-    public  String longestString(int n, String[] arr) {
+    public  String longestString(String[] arr) {
         Trie obj = new Trie();
+        int n = arr.length ; 
         for(int i = 0 ; i < n ; i++){
             obj.insert(arr[i]); 
         }
