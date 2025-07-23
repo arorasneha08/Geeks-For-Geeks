78ce785629ed4517698a705e5a63f59a4f156e1c
arorasneha08 <158125379+arorasneha08@users.noreply.github.com>
2025-07-23 09:53:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of Subarrays/sum-of-subarrays.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int subarraySum(int[] arr) {
+        int n = arr.length ; 
+        int sum = 0 ; 
+        for(int i = 0 ; i < n ; i++){
+            int start = i+1;  
+            int end = n-i ; 
+            int contri = start * end; 
+            sum += (arr[i] * contri); 
+        }
+        return sum ; 
+    }
+}
