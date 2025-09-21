25282b3b954abeef5d87193f22e50eabdaeb9cd7
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2025-09-21 15:45:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max rectangle/max-rectangle.java
+++ Difficulty: Hard/Max rectangle/max-rectangle.java
@@ -1,28 +1,3 @@
-//{ Driver Code Starts
-import java.util.*;
-
-class FindMinCost {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t > 0) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            int arr[][] = new int[n][m];
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    arr[i][j] = sc.nextInt();
-                }
-            }
-            System.out.println(new Solution().maxArea(arr));
-            t--;
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
 
 
 /*Complete the function given below*/
