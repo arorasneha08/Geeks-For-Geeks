057c6619b76822c4681b5d0a390fca5ee5cae7ee
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2025-09-25 01:27:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Generate Binary Numbers/generate-binary-numbers.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ArrayList<String> generateBinary(int n) {
+        ArrayList<String> res = new ArrayList<>(); 
+        Queue<String> q = new LinkedList<>(); 
+        q.offer("1") ;
+        while(n > 0){
+            String curr = q.peek(); 
+            res.add(q.poll()); 
+            q.offer(curr + "0"); 
+            q.offer(curr + "1"); 
+            n-- ; 
+        }
+        return res ; 
+    }
+}
