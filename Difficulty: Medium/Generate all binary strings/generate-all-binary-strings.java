1af7b5f3ee53b863c368e3cc0dff783dbab7d1bf
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2025-09-30 10:47:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Generate all binary strings/generate-all-binary-strings.java
+++ Difficulty: Medium/Generate all binary strings/generate-all-binary-strings.java
@@ -1,52 +1,20 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-import java.util.*;
-import java.lang.*;
-import java.math.*;
-import java.io.*;
-
-class GFG {
-  public static void main(String[] args) throws IOException {
-    Scanner sc = new Scanner(System.in);
-    int T = sc.nextInt();
-    while (T-- > 0) {
-      int n = sc.nextInt();
-      Solution obj = new Solution();
-      List<String> ans = obj.generateBinaryStrings(n);
-      for(String s : ans) System.out.print(s+" ");
-      System.out.println();
-    
-System.out.println("~");
-}
-  }
-}
-
-// } Driver Code Ends
-
-
-//User function Template for Java
-
 class Solution {
-    private static void func(int i , char ch[], List<String> res, StringBuilder str , int n){
-        if(i == n) {
-            res.add(str.toString());
+    private void func(int index , char[] bit , ArrayList<String> list , StringBuilder str, int n){
+        if(index == n) {
+            list.add(str.toString());
             return ; 
         }
-        for(char c : ch){
-            if(i > 0 && str.charAt(i-1) == '1' && c == '1') continue ;
+        for(char c : bit){
             str.append(c);
-            func(i + 1 , ch, res, str , n); 
+            func(index + 1 , bit, list, str , n); 
             str.deleteCharAt(str.length()-1); 
         }
     }
-    public static List<String> generateBinaryStrings(int n) {
-        List<String> res = new ArrayList<>(); 
+    public ArrayList<String> binstr(int n) {
+        char bit[] = {'0', '1'}; 
+        ArrayList<String> list = new ArrayList<>(); 
         StringBuilder str = new StringBuilder();
-        char ch[]= {'0', '1'};
-        func(0, ch , res , str , n); 
-        return res; 
+        func(0 , bit , list , str, n);
+        return list;
     }
 }
-     
-     
