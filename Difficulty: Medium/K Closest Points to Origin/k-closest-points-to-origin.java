bd1ba9219ec130c702fc7fa27ed36ae689753e2a
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2025-10-23 08:52:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.java
+++ Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.java
@@ -1,69 +1,37 @@
-//{ Driver Code Starts
-import java.util.*;
-
-
-// } Driver Code Ends
 class Solution {
-    public class Pair implements Comparable<Pair>{
+    public class Pair implements Comparable<Pair> {
         int distance;
-        int[] point ;
-        Pair(int distance , int point[]){
-            this.distance = distance;  
-            this.point = point ;
+        int[] point;
+
+        Pair(int distance, int[] point) {
+            this.distance = distance;
+            this.point = point;
         }
+
         public int compareTo(Pair other) {
-            return this.distance - other.distance;  
+            return this.distance - other.distance;
         }
     }
-    public int[][] kClosest(int[][] points, int k) {
+
+    public ArrayList<ArrayList<Integer>> kClosest(int[][] points, int k) {
         PriorityQueue<Pair> pq = new PriorityQueue<>();
-        int n = points.length ;
-        for(int i = 0 ; i< n; i++){
+        int n = points.length;
+
+        for (int i = 0; i < n; i++) {
             int distance = points[i][0] * points[i][0] + points[i][1] * points[i][1];
-            pq.offer(new Pair(distance , points[i])); 
+            pq.offer(new Pair(distance, points[i]));
         }
-        int[][] res = new int[k][2]; 
-        int index = 0 ; 
-        while(k > 0 && !pq.isEmpty()){
-            res[index++] = pq.poll().point ; 
+
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+
+        while (k > 0 && !pq.isEmpty()) {
+            int[] p = pq.poll().point;
+            ArrayList<Integer> temp = new ArrayList<>();
+            temp.add(p[0]);
+            temp.add(p[1]);
+            res.add(temp);
             k--;
-       }
-       return res ; 
-    }
-}
-
-//{ Driver Code Starts.
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int t = scanner.nextInt();
-
-        while (t-- > 0) {
-            int k = scanner.nextInt();
-            int n = scanner.nextInt();
-            int[][] points = new int[n][2];
-            for (int i = 0; i < n; i++) {
-                points[i][0] = scanner.nextInt();
-                points[i][1] = scanner.nextInt();
-            }
-            Solution solution = new Solution();
-            int[][] ans = solution.kClosest(points, k);
-
-            Arrays.sort(ans, (a, b) -> {
-                if (a[0] != b[0]) {
-                    return Integer.compare(a[0], b[0]);
-                }
-                return Integer.compare(a[1], b[1]);
-            });
-            for (int[] point : ans) {
-                System.out.println(point[0] + " " + point[1]);
-            }
-            System.out.println("~");
         }
-
-        scanner.close();
+        return res;
     }
 }
-// } Driver Code Ends
