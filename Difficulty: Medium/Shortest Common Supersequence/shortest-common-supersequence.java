6743e627c12a17cba31c00a866c3050c673493f7
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2025-11-11 13:48:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.java
+++ Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.java
@@ -1,30 +1,24 @@
-// User function Template for Java
-
 class Solution {
-    public static int shortestCommonSupersequence(String s1, String s2) {
+    private static int func(int i , int j , String s1 , String s2 , int dp[][]){
+        if(i == 0 || j == 0) return 0 ; 
+        if(dp[i][j] != -1) return dp[i][j]; 
+        
+        if(s1.charAt(i-1) == s2.charAt(j-1)){
+            dp[i][j] = 1 + func(i - 1 , j -1 , s1 , s2 , dp); 
+        }
+        else{
+            dp[i][j] = Math.max(func(i -1 , j , s1 , s2 , dp) , func(i , j - 1 , s1 , s2 , dp));
+        }
+        return dp[i][j]; 
+    }
+    public static int minSuperSeq(String s1, String s2) {
         int n = s1.length(); 
-        int m = s2.length() ; 
+        int m = s2.length(); 
         int dp[][] = new int[n+1][m+1]; 
         for(int row[] : dp){
-            Arrays.fill(row , -1);
+            Arrays.fill(row , -1); 
         }
-        for(int i = 0 ; i < n ; i++){
-            dp[i][0]= 0 ; 
-        }
-        for(int j = 0; j < m ;j++){
-            dp[0][j] = 0 ; 
-        }
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= m ; j++){
-                if(s1.charAt(i-1) == s2.charAt(j-1)){
-                    dp[i][j] = 1 + dp[i-1][j-1]; 
-                }
-                else{
-                    dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]); 
-                }
-            }
-        }
-        int lcs = dp[n][m]; 
-        return n + m - lcs; 
+        int lcs =  func(n , m , s1 , s2 , dp);
+        return n + m - lcs ; 
     }
-}
+} 
