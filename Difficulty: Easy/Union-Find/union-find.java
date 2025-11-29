b738485382c5c64b0d66a46921a09c68adc40904
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2025-11-29 23:05:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Union-Find/union-find.java
+++ Difficulty: Easy/Union-Find/union-find.java
@@ -1,102 +1,28 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-import java.io.*;
-
-public class GFG {
-    public static void main(String[] args) throws IOException {
-        
-        //using BufferedReader to take input
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        //taking total testcases
-        int t = Integer.parseInt(br.readLine());
-
-        while(t-- > 0){
-            
-            //taking total number of elements
-            int n = Integer.parseInt(br.readLine());
-            
-            int par[] = new int[n+1];
-            int rank[] = new int[n+1];
-            
-            //initializing the parent and
-            //the rank array
-            for(int i = 1; i<=n; ++i){
-                par[i] = i;
-                rank[i] = 1;
-            }
-            int a = 0, b = 0;
-            char c = '0';
-            
-            //taking number of queries
-            int query = Integer.parseInt(br.readLine());
-            
-            Boolean output = false;
-            while(query-- >0){
-                String str[] = br.readLine().trim().split(" ");
-                c = str[0].charAt(0);
-                a = Integer.parseInt(str[1]);
-                b = Integer.parseInt(str[2]);
-
-                //if query type is 'U'
-                //then calling union_ method
-                if(c == 'U'){
-                    new Solution().union_(a, b, par, rank);
-                }
-                else{//else calling isConnected() method
-                    output = new Solution().isConnected(a, b, par, rank);
-                    if(output == true)
-                        System.out.println("1");
-                    else
-                        System.out.println("0");
-                }
-            }
-
-
-
-        
-System.out.println("~");
-}
-    }
-}
-// } Driver Code Ends
-
-
-//User function Template for Java
-
-
 class Solution {
-    private int findParent(int a , int par[]){
-        if(par[a] == a) return a ;
-        int ul_parent = findParent(par[a] , par) ;
-        par[a] = ul_parent; 
-        return ul_parent; 
-    }
-    public void union_(int a, int b, int par[], int rank[]){
-        int parent_a = findParent(a, par);
-        int parent_b = findParent(b, par);
-        
-        if(rank[parent_a] > rank[parent_b]){
-            par[parent_b] = parent_a ;  
+    int find(int x , int par[]){
+        if(par[x] != x){
+            par[x] = find(par[x] , par); 
         }
-        else if(rank[parent_a] < rank[parent_b]){
-            par[parent_a] = parent_b; 
+        return par[x]; 
+    }
+    public void union_(int a, int b, int par[], int rank[]) {
+        int pu = find(a , par); 
+        int pv = find(b , par); 
+        if(pu == pv) return ; 
+        
+        if(rank[pu] < rank[pv]){
+            par[pu] = pv ; 
+        }
+        else if(rank[pu] > rank[pv]){
+            par[pv] = pu ; 
         }
         else{
-            par[parent_a] = parent_b; 
-            rank[parent_b] += 1  ; 
+            par[pv] = pu ; 
+            rank[pu] ++ ; 
         }
     }
 
-    //Function to check whether 2 nodes are connected or not.
-    public Boolean isConnected(int a, int b, int par[], int rank[]){
-        int parent_a = findParent(a , par);
-        int parent_b = findParent(b , par);
-        if(parent_a == parent_b){
-            return true ; 
-        }
-        return false; 
+    public Boolean isConnected(int a, int b, int par[], int rank[]) {
+        return find(a , par) == find(b , par);
     }
-
 }
