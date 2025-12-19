eecfd9a4e55b61ec28aaf3663e416a26fcfd70b8
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2025-12-19 13:12:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bus Conductor/bus-conductor.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findMoves(int[] chairs, int[] passengers) {
+        Arrays.sort(chairs); 
+        Arrays.sort(passengers); 
+        int n = chairs.length ; 
+        int sum = 0; 
+        for(int i = 0 ; i < n ; i++){
+            sum += Math.abs(chairs[i] - passengers[i]); 
+        }
+        return sum; 
+    }
+}
