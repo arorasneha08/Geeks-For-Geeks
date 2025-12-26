7d5b028b9efaba80eb0da72e2cd8728836dc4530
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2025-12-27 00:23:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pair cube count/pair-cube-count.java
@@ -0,0 +1,17 @@
+// User function Template for Java
+
+class Solution {
+    static int pairCubeCount(int n) {
+        // code here
+        int count = 0 ; 
+        for(int i = 1  ; i <= Math.cbrt(n) ; i++){
+            int a = i * i * i ; 
+            int diff = n - a ; 
+            int cbrtDiff = (int) Math.cbrt(diff);
+            if(cbrtDiff * cbrtDiff * cbrtDiff == diff){
+                count ++; 
+            }
+        }
+        return count; 
+    }
+};
