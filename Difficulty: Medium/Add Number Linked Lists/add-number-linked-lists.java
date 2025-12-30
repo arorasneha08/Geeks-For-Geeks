3c3caeb21aab46868bdf6d5d741a78a28a95870d
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2025-12-30 10:18:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Add Number Linked Lists/add-number-linked-lists.java
+++ Difficulty: Medium/Add Number Linked Lists/add-number-linked-lists.java
@@ -1,11 +1,3 @@
-//{ Driver Code Starts
-// driver
-
-import java.io.*;
-import java.util.*;
-
-
-// } Driver Code Ends
 /* node for linked list
 
 class Node {
@@ -42,8 +34,7 @@ class Solution {
         Node head = null;
         Node tail = head;
         
-        while(rev1 != null || rev2 != null || carry != 0)
-        {
+        while(rev1 != null || rev2 != null || carry != 0){
             int val1 = 0;
             int val2 = 0;
             if(rev1 != null) val1 = rev1.data;
@@ -55,13 +46,11 @@ class Solution {
             carry = sum / 10;
             Node temp = new Node(digit);
             
-            if(head == null)
-            {
+            if(head == null) {
                 head = temp;
                 tail = temp;
             }
-            else
-            {
+            else {
                 tail.next = temp;
                 tail = temp;
             }
@@ -73,73 +62,11 @@ class Solution {
         
         Node result = reverse(head);
         
-        while(result != null)
-        {
-            if(result.data != 0)
-            break;
+        while(result != null){
+            if(result.data != 0) break;
             result = result.next;
         }
         
         return result;
     }
 }
-
-//{ Driver Code Starts.
-
-class Node {
-    int data;
-    Node next;
-
-    Node(int d) {
-        data = d;
-        next = null;
-    }
-}
-
-class GfG {
-
-    static void printList(Node n) {
-        while (n != null) {
-            System.out.print(n.data + " ");
-            n = n.next;
-        }
-        System.out.println();
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(read.readLine());
-        while (T-- > 0) {
-
-            String str[] = read.readLine().trim().split(" ");
-            int n = str.length;
-
-            Node num1 = new Node(Integer.parseInt(str[0]));
-            Node tail = num1;
-            for (int i = 1; i < n; i++) {
-                int val = Integer.parseInt(str[i]);
-                tail.next = new Node(val);
-                tail = tail.next;
-            }
-
-            String str2[] = read.readLine().trim().split(" ");
-            int m = str2.length;
-
-            Node num2 = new Node(Integer.parseInt(str2[0]));
-            tail = num2;
-            for (int i = 1; i < m; i++) {
-
-                tail.next = new Node(Integer.parseInt(str2[i]));
-                tail = tail.next;
-            }
-
-            Solution g = new Solution();
-            Node res = g.addTwoLists(num1, num2);
-            printList(res);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
