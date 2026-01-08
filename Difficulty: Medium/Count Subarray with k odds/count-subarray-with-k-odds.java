83f4a513db45db55fab1106d8271d5ada27d9a25
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2026-01-08 09:43:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Subarray with k odds/count-subarray-with-k-odds.java
+++ Difficulty: Medium/Count Subarray with k odds/count-subarray-with-k-odds.java
@@ -1,84 +1,27 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-
-class IntArray
-{
-    public static int[] input(BufferedReader br, int n) throws IOException
-    {
-        String[] s = br.readLine().trim().split(" ");
-        int[] a = new int[n];
-        for(int i = 0; i < n; i++)
-            a[i] = Integer.parseInt(s[i]);
-
-        return a;
-    }
-
-    public static void print(int[] a)
-    {
-        for(int e : a)
-            System.out.print(e + " ");
-        System.out.println();
-    }
-
-    public static void print(ArrayList<Integer> a)
-    {
-        for(int e : a)
-            System.out.print(e + " ");
-        System.out.println();
-    }
-}
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t;
-        t = Integer.parseInt(br.readLine());
-        while(t-- > 0){
-            
-            int n;
-            n = Integer.parseInt(br.readLine());
-            
-            
-            int[] nums = IntArray.input(br, n);
-            
-            
-            int k;
-            k = Integer.parseInt(br.readLine());
-            
-            Solution obj = new Solution();
-            int res = obj.countSubarray(n, nums, k);
-            
-            System.out.println(res);
-            
-        
-System.out.println("~");
-}
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    public static int helper(int nums[] , int k , int n){
-        int count = 0 ;
-        int left = 0 , sum = 0 ; 
-        
-        for(int right = 0 ; right < n ; right ++){
-            sum += nums[right] % 2;
-            
-            while(sum > k){
-                sum -= nums[left] % 2; 
-                left ++ ; 
+
+    
+    private int atMostK(int[] arr, int k) {
+        int left = 0, count = 0, oddCount = 0;
+
+        for (int right = 0; right < arr.length; right++) {
+            if (arr[right] % 2 == 1) {
+                oddCount++;
             }
-            count += (right - left + 1); 
+
+            while (oddCount > k) {
+                if (arr[left] % 2 == 1) {
+                    oddCount--;
+                }
+                left++;
+            }
+
+            count += (right - left + 1);
         }
-        return count ; 
+
+        return count;
     }
-    public static int countSubarray(int n, int[] nums, int k) {
-        return helper(nums , k , n) - helper(nums , k-1, n); 
+    public int countSubarrays(int[] arr, int k) {
+        return atMostK(arr, k) - atMostK(arr, k - 1);
     }
 }
-        
