d1b3115ea2ac5a6a00018361e09ba65a7cf1d157
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2026-01-14 12:23:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Police and Thieves/police-and-thieves.java
+++ Difficulty: Medium/Police and Thieves/police-and-thieves.java
@@ -1,26 +1,34 @@
 class Solution {
     public int catchThieves(char[] arr, int k) {
-        int n = arr.length ;
-        int i = 0 , j = 0 ; 
-        int count = 0; 
-        while(i < n && j < n){
-            while(i < n && arr[i] != 'P') i ++; 
-            while(j < n && arr[j] != 'T') j++; 
-            
-            if(i < n && j < n){
-                if(Math.abs(i-j) <= k){
-                    count ++; 
-                    i ++; 
-                    j ++ ; 
-                }
-                else if(i < j){
-                    i++ ; 
-                }
-                else{
-                    j ++; 
-                }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == 'T'){
+                pq.offer(i);
             }
         }
-        return count; 
+        
+        int cnt = 0;
+        for(int i=0; i<arr.length; i++){
+            if(pq.size() <= 0) break;
+            
+            if(arr[i] == 'P'){
+                int idx = pq.peek();
+                if(idx > i+k) continue;
+            
+                while(idx <= i+k){
+                    idx = pq.poll();
+                    if(Math.abs(i - idx) <= k){
+                        cnt++;
+                        break;
+                    }
+                }
+                 
+            }
+        }
+        
+        return cnt;
+        
+        
     }
 }
