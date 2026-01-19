7bf67102b98a783b1e8d7b69a57852a480ab3358
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2026-01-19 10:01:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Remove K Digits/remove-k-digits.java
+++ Difficulty: Medium/Remove K Digits/remove-k-digits.java
@@ -1,31 +1,9 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GfG {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t-- > 0) {
-            String S = sc.next();
-            int K = sc.nextInt();
-            Solution obj = new Solution();
-            System.out.println(obj.removeKdigits(S, K));
-        
-System.out.println("~");
-}
-    }
-}
-
-// } Driver Code Ends
 
 
 // User function Template for Java
 
 class Solution {
-    public String removeKdigits(String str, int k) {
+    public String removeKdig(String str, int k) {
         int n = str.length();
         Stack<Character> st = new Stack<>();
         
