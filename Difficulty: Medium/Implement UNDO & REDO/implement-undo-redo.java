231aa3a4bb7d60eba273d9892d3961811e416214
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2026-01-20 10:32:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement UNDO & REDO/implement-undo-redo.java
@@ -0,0 +1,29 @@
+class Solution {
+    Stack<Character> undo = new Stack<>(); 
+    Stack<Character> redo = new Stack<>(); 
+    StringBuilder str = new StringBuilder(); 
+    
+    public void append(char x) {
+        str.append(x); 
+        undo.push(x); 
+        redo.clear(); 
+    }
+
+    public void undo() {
+        if(str.length() == 0) return ; 
+        char remove = str.charAt(str.length()-1); 
+        str.deleteCharAt(str.length()-1); 
+        redo.push(remove); 
+    }
+
+    public void redo() {
+        if(redo.isEmpty()) return ; 
+        char ch = redo.pop(); 
+        str.append(ch); 
+        undo.push(ch); 
+    }
+
+    public String read() {
+        return str.toString(); 
+    }
+}
