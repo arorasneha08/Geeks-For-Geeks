91ed1a2888e57a7042505aff716d464b304639c1
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2026-01-31 11:33:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Implement k Queues in a Single Array/implement-k-queues-in-a-single-array.java
@@ -0,0 +1,38 @@
+class kQueues {
+    HashMap<Integer , Queue<Integer>> mpp = new HashMap<>(); 
+    int size ; 
+    int n ; 
+    int k ; 
+    
+    kQueues(int n, int k) {
+        this.size = 0 ; 
+        this.n = n ; 
+        this.k = k ; 
+    }
+
+    void enqueue(int x, int i) {
+        if(isFull()) return ; 
+        if(!mpp.containsKey(i)){
+            mpp.put(i , new LinkedList<>());
+        }
+        mpp.get(i).add(x); 
+        size ++; 
+    }
+
+    int dequeue(int i) {
+        Queue<Integer> q = mpp.get(i); 
+        if(q == null || q.isEmpty()) return -1;
+        size -- ; 
+        return q.poll(); 
+    }
+
+    boolean isEmpty(int i) {
+        Queue<Integer> q = mpp.get(i); 
+        return q == null || q.size() == 0 ; 
+    }
+
+    boolean isFull() {
+        if(size == n) return true; 
+        return false;
+    }
+}
