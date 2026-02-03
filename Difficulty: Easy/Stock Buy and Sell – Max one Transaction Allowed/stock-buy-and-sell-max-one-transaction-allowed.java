743b876ebcaefc310acd81de02942479ba81bfa7
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2026-02-03 15:53:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.java
+++ Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.java
@@ -1,46 +1,17 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String arr[] = br.readLine().split(" ");
-            int prices[] = new int[arr.length];
-
-            for (int i = 0; i < arr.length; i++) {
-                prices[i] = Integer.parseInt(arr[i]);
-            }
-            Solution obj = new Solution();
-            int res = obj.maximumProfit(prices);
-            System.out.println(res);
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    public int maximumProfit(int prices[]) {
-        int n = prices.length ;
-        int profit = 0 ;
-        int buy = prices[0]; 
-        for(int i = 0 ; i<n ; i++){
-            if(prices[i] < buy){
-                buy = prices[i] ; 
-            }
-            if(prices[i] - buy > profit){
-                profit = prices[i] - buy ; 
-            }
+     static int maxProfit(int []prices) {
+    int buy = prices[0], sell = prices[0], profit = 0;
+
+    for (int i = 0; i < prices.length; i++) {
+        if (buy > prices[i]) {
+            buy = prices[i];
+            sell = prices[i];
         }
-        return profit ;
+        if (sell < prices[i]) {
+            sell = prices[i];
+        }
+        profit = Math.max(profit, sell - buy);
     }
+    return profit;
+}
 }
