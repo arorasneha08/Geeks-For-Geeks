d4ee105d9d07dd6e22af93d2cc3128a1ae94c71e
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2026-02-05 10:51:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximize Number of 1's/maximize-number-of-1s.java
+++ Difficulty: Medium/Maximize Number of 1's/maximize-number-of-1s.java
@@ -1,51 +1,25 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    // Driver code
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            String[] str = br.readLine().trim().split(" ");
-            int[] arr = new int[str.length];
-
-            for (int i = 0; i < str.length; i++) {
-                arr[i] = Integer.parseInt(str[i]);
-            }
-            int m = Integer.parseInt(br.readLine().trim());
-            int ans = new Solution().maxOnes(arr, m);
-
-            System.out.println(ans);
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    // k is the maximum number of zeros allowed to flip
     public int maxOnes(int arr[], int k) {
         int n = arr.length ; 
-        int maxLen = 0 ;
         int zeros = 0 ; 
         int left = 0 ; 
+        int max = 0 ; 
         
-        for(int right = 0 ; right < n ; right ++){
-            if(arr[right] == 0 ){
-                zeros ++ ; 
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] == 0){
+                zeros ++; 
             }
-            while(zeros > k){
-                if(arr[left] == 0) zeros -- ; 
-                left ++ ; 
+            if(zeros > k){
+              if(arr[left] == 0){
+                  zeros --; 
+              }  
+              left ++; 
             }
-            maxLen = Math.max(maxLen , right - left + 1) ; 
+            if(zeros <= k){
+                int len = i - left + 1; 
+                max = Math.max(max , len); 
+            }
         }
-        return maxLen ; 
+        return max; 
     }
 }
