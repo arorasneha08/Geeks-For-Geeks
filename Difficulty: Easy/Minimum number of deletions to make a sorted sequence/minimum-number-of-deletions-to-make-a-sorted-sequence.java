79dba6ea0723cb663e7cbc79582ca5f08f598db7
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2026-02-20 22:25:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum number of deletions to make a sorted sequence/minimum-number-of-deletions-to-make-a-sorted-sequence.java
@@ -0,0 +1,30 @@
+
+class Solution {
+    public int minDeletions(int arr[], int n) {
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int num : arr) {
+            set.add(num);
+        }
+
+        List<Integer> list = new ArrayList<>(set);
+
+        int m = list.size();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (arr[i - 1] == list.get(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        int lisLength = dp[n][m];
+
+        return n - lisLength;
+    }
+}
