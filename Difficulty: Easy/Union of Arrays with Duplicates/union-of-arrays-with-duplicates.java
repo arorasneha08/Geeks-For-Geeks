423643148f54624384b3b2917c91169ebd99884f
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2026-02-23 10:56:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.java
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.java
@@ -1,50 +1,5 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine().trim());
-
-        while (t-- > 0) {
-            // Reading the first array
-            String line1 = read.readLine().trim();
-            String[] numsStr1 = line1.split(" ");
-            int[] a = new int[numsStr1.length];
-            for (int i = 0; i < numsStr1.length; i++) {
-                a[i] = Integer.parseInt(numsStr1[i]);
-            }
-
-            // Reading the second array
-            String line2 = read.readLine().trim();
-            String[] numsStr2 = line2.split(" ");
-            int[] b = new int[numsStr2.length];
-            for (int i = 0; i < numsStr2.length; i++) {
-                b[i] = Integer.parseInt(numsStr2[i]);
-            }
-
-            // Creating an instance of the Solution class
-            Solution ob = new Solution();
-
-            // Calling doUnion method and printing the result
-            System.out.println(ob.findUnion(a, b));
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    public static int findUnion(int a[], int b[]) {
+    public static ArrayList<Integer> findUnion(int[] a, int[] b) {
         Arrays.sort(a);
         Arrays.sort(b); 
        int n1 = a.length ; 
@@ -85,6 +40,6 @@ class Solution {
            }
            j++ ; 
        }
-       return ans.size(); 
+       return ans;
     }
 }
