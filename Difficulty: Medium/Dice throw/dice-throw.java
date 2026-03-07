02cdb5efab93c24ae96ca4b7b6c00fbcc17d4e80
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2026-03-07 18:02:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dice throw/dice-throw.java
+++ Difficulty: Medium/Dice throw/dice-throw.java
@@ -1,19 +1,25 @@
 class Solution {
-    static int func(int faces , int dice , int target , int dp[][]){
-        if(dice == 0 && target == 0) return 1; 
-        if(dice == 0 || target < 0) return 0 ;
-        if(dp[dice][target] != -1) return dp[dice][target]; 
-        int res = 0 ; 
-        for(int i = 1 ; i <= faces ; i++){
-            res += func(faces , dice - 1 , target - i , dp); 
+    static int count(int m , int n , int x , int dp[][]){
+        if(x < 0) return 0 ;
+        if(n == 0){
+            if(x == 0) return 1 ; 
+            else return 0 ; 
         }
-        return dp[dice][target] = res ; 
+        if(dp[n][x] != -1){
+            return dp[n][x]; 
+        }
+        int ways = 0 ;
+        for(int i = 1 ; i<= m ; i++){
+            ways += count(m , n-1 , x-i , dp); 
+        }
+        dp[n][x] = ways;  
+        return ways; 
     }
     static int noOfWays(int m, int n, int x) {
-        int dp[][] = new int[51][51]; 
+        int dp[][] = new int[n+1][x+1];
         for(int row[] : dp){
-            Arrays.fill(row , -1);
+            Arrays.fill(row , -1); 
         }
-        return func(m , n , x , dp); 
+        return count(m, n, x, dp); 
     }
 };
