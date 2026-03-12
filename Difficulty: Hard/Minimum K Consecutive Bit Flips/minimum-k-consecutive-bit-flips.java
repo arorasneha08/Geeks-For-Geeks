49df61a02b80879291bf05f7acf9d10878115fd0
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2026-03-12 09:07:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimum K Consecutive Bit Flips/minimum-k-consecutive-bit-flips.java
+++ Difficulty: Hard/Minimum K Consecutive Bit Flips/minimum-k-consecutive-bit-flips.java
@@ -1,36 +1,27 @@
 class Solution {
     public int kBitFlips(int[] arr, int k) {
-        int n = arr.length ; 
-        int pref[] = new int[n+2]; 
-        int res = 0 ;
-        
-        for(int i = 0 ; i <= (n-k) ; i++){
-            if(i > 0){
-                pref[i] += pref[i-1]; 
+        int n = arr.length;
+        int[] isFlipped = new int[n];
+        int flip = 0;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (i >= k) {
+                flip ^= isFlipped[i - k];
             }
-            
-            if(arr[i] == 0 && pref[i] % 2 == 0){
-                pref[i] ++; 
-                pref[i+k]-- ; 
-                res ++ ; 
-            }
-            
-            if(arr[i] == 1 && pref[i] % 2 == 1){
-                pref[i] ++; 
-                pref[i+k] -- ; 
-                res ++; 
-            }
-            
-        }
-        for(int i = n-k+1 ; i< n ; i++){
-            pref[i] += pref[i-1] ;
-            if(arr[i] == 0 && pref[i] % 2 == 0 ){
-                return -1; 
-            }
-            if(arr[i] == 1 && pref[i] % 2 == 1){
-                return -1; 
+
+            if ((arr[i] ^ flip) == 0) {
+                if (i + k > n) {
+                    return -1;
+                }
+
+                ans++;
+                flip ^= 1;
+                isFlipped[i] = 1;
             }
         }
-        return res; 
+
+        return ans;
     }
 }
