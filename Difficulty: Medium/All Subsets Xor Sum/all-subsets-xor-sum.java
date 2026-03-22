3604bee580d0e6a6c11b125cf78d8d4fe4296ca6
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2026-03-22 15:53:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/All Subsets Xor Sum/all-subsets-xor-sum.java
+++ Difficulty: Medium/All Subsets Xor Sum/all-subsets-xor-sum.java
@@ -1,19 +1,25 @@
 class Solution {
-    Integer[][] dp;
-
-    int func(int idx, int xor, int[] arr) {
-        if (idx == arr.length) return xor;
-
-        if (dp[idx][xor] != null) return dp[idx][xor];
-
-        int pick = func(idx + 1, xor ^ arr[idx], arr);
-        int notpick = func(idx + 1, xor, arr);
-
-        return dp[idx][xor] = pick + notpick;
+    int func(int idx , int xor , int arr[] , int dp[][]){
+        if(idx == arr.length) return xor; 
+        if(dp[idx][xor] != -1) return dp[idx][xor]; 
+        
+        int nottake = func(idx + 1 , xor , arr , dp); 
+        int take = func(idx + 1 , xor ^ arr[idx] , arr , dp); 
+        return dp[idx][xor ] = take + nottake; 
     }
-
-    int subsetXORSum(int[] arr) {
-        dp = new Integer[arr.length][2048]; 
-        return func(0, 0, arr);
+    int subsetXORSum(int arr[]) {
+        int n = arr.length ; 
+        int total = 0 ; 
+        int max = 0 ; 
+        
+        for(int num : arr){
+            total += num ; 
+        }
+        int dp[][]=  new int[n + 1][total + 1]; 
+        for(int row[] : dp){
+            Arrays.fill(row , -1); 
+        }
+        
+        return func(0 , 0 , arr , dp); 
     }
 }
