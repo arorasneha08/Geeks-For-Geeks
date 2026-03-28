e1e1cae0fac0d2db159b84f2f336f57e12776db4
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2026-03-28 10:31:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Articulation Point - II/articulation-point-ii.java
+++ Difficulty: Hard/Articulation Point - II/articulation-point-ii.java
@@ -1,96 +1,73 @@
-//{ Driver Code Starts
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt();
-        while (tc-- > 0) {
-            int V = sc.nextInt();
-            int E = sc.nextInt();
-            int[][] edges = new int[E][2];
-            for (int i = 0; i < E; i++) {
-                edges[i][0] = sc.nextInt();
-                edges[i][1] = sc.nextInt();
-            }
-
-            Solution obj = new Solution();
-            ArrayList<Integer> ans = obj.articulationPoints(V, edges);
-            Collections.sort(ans);
-            for (int val : ans) {
-                System.out.print(val + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
     static ArrayList<Integer> articulationPoints(int V, int[][] edges) {
-        // Create adjacency list
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < V; i++)
-            adj.add(new ArrayList<>());
-        
-        for (int[] edge : edges) {
-            int u = edge[0], v = edge[1];
+        for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
+
+        for (int[] e : edges) {
+            int u = e[0], v = e[1];
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
 
-        int[] time = {0}; // Global time counter
-        int[] disc = new int[V]; // Discovery times
-        int[] low = new int[V];  // Low-link values
-        boolean[] visited = new boolean[V];
-        boolean[] isAP = new boolean[V]; // Articulation point flags
+        int[] disc = new int[V];
+        int[] low = new int[V];
+        int[] parent = new int[V];
+        int[] childCount = new int[V];
+        int[] itIndex = new int[V];
+        boolean[] ap = new boolean[V];
 
-        // Initialize discovery and low arrays
-        Arrays.fill(disc, -1);
-        Arrays.fill(low, -1);
+        Arrays.fill(parent, -1);
 
-        for (int i = 0; i < V; i++) {
-            if (!visited[i]) {
-                dfs(i, -1, time, disc, low, visited, isAP, adj);
-            }
-        }
+        int time = 1;
+        Deque<Integer> stack = new ArrayDeque<>();
 
-        ArrayList<Integer> result = new ArrayList<>();
-        for (int i = 0; i < V; i++) {
-            if (isAP[i]) result.add(i);
-        }
+        for (int start = 0; start < V; start++) {
+            if (disc[start] != 0) continue;
 
-        if (result.size() == 0) result.add(-1);
-        return result;
-    }
+            stack.push(start);
 
-    static void dfs(int u, int parent, int[] time, int[] disc, int[] low,
-                    boolean[] visited, boolean[] isAP, ArrayList<ArrayList<Integer>> adj) {
-        visited[u] = true;
-        disc[u] = low[u] = time[0]++;
-        int children = 0;
+            while (!stack.isEmpty()) {
+                int u = stack.peek();
 
-        for (int v : adj.get(u)) {
-            if (v == parent) continue;
-
-            if (!visited[v]) {
-                dfs(v, u, time, disc, low, visited, isAP, adj);
-                low[u] = Math.min(low[u], low[v]);
-                children++;
-
-                if (parent != -1 && low[v] >= disc[u]) {
-                    isAP[u] = true;
+                if (disc[u] == 0) {
+                    disc[u] = low[u] = time++;
                 }
-            } else {
-                low[u] = Math.min(low[u], disc[v]);
+
+                if (itIndex[u] < adj.get(u).size()) {
+                    int v = adj.get(u).get(itIndex[u]++);
+
+                    if (v == parent[u]) continue;
+
+                    if (disc[v] == 0) {
+                        parent[v] = u;
+                        childCount[u]++;
+                        stack.push(v);
+                    } else {
+                        low[u] = Math.min(low[u], disc[v]);
+                    }
+                } else {
+                    stack.pop();
+
+                    int p = parent[u];
+                    if (p == -1) {
+                        if (childCount[u] > 1) ap[u] = true;
+                    } else {
+                        low[p] = Math.min(low[p], low[u]);
+                        if (parent[p] != -1 && low[u] >= disc[p]) {
+                            ap[p] = true;
+                        }
+                    }
+                }
             }
         }
 
-        // Special case for root node
-        if (parent == -1 && children > 1) {
-            isAP[u] = true;
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            if (ap[i]) ans.add(i);
         }
+
+        if (ans.isEmpty()) ans.add(-1);
+        return ans;
     }
 }
+
