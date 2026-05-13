e2601af2937c162425f54372cdc00fa88ef2f8bb
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2026-05-13 11:11:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Mother Vertex/mother-vertex.java
+++ Difficulty: Medium/Mother Vertex/mother-vertex.java
@@ -1,28 +1,45 @@
-
 class Solution {
-    private void dfs(int node , int visited[],ArrayList<ArrayList<Integer>> adj){
-        visited[node] = 1; 
+    
+    void dfs(int node, ArrayList<ArrayList<Integer>> adj, boolean[] vis) {
+        vis[node] = true;
         
-        for(int adjNode: adj.get(node)){
-            if(visited[adjNode] == 0){
-                dfs(adjNode , visited , adj); 
+        for(int next : adj.get(node)) {
+            if(!vis[next]) {
+                dfs(next, adj, vis);
             }
         }
     }
-    public int findMotherVertex(int V, ArrayList<ArrayList<Integer>> adj) {
-        int visited[] = new int[V] ; 
-        int lastNode = 0;
-        for(int i = 0 ; i < V ; i++){
-            if(visited[i] == 0){
-                dfs(i , visited , adj); 
-                lastNode = i ; 
-            }    
+    
+    public int findMotherVertex(int V, int[][] edges) {
+        
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        
+        for(int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
         }
-        visited = new int[V]; 
-        dfs(lastNode , visited , adj); 
-        for(int i = 0 ; i < V ;i++){
-            if(visited[i] == 0) return -1; 
+        
+        for(int[] e : edges) {
+            adj.get(e[0]).add(e[1]);
         }
-        return lastNode ; 
+        
+        boolean[] vis = new boolean[V];
+        int candidate = -1;
+        
+        for(int i = 0; i < V; i++) {
+            if(!vis[i]) {
+                dfs(i, adj, vis);
+                candidate = i;
+            }
+        }
+                vis = new boolean[V];
+        dfs(candidate, adj, vis);
+        
+        for(int i = 0; i < V; i++) {
+            if(!vis[i]) {
+                return -1;
+            }
+        }
+        
+        return candidate;
     }
 }
