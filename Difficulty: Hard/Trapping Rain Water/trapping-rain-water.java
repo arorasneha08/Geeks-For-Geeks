28c900d79f45c44e7f50b18e3fb91dbcf947dc98
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2026-07-28 01:28:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Trapping Rain Water/trapping-rain-water.java
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.java
@@ -1,25 +1,45 @@
 class Solution {
     public int maxWater(int arr[]) {
         int n = arr.length ; 
-        int left = 0 , right = n-1 ; 
-        int leftMax = 0 , rightMax = 0 ; 
-        int ans = 0 ; 
+        // int prefMax[] = new int[n];
+        // int suffMax[] = new int[n]; 
+        
+        // prefMax[0] = arr[0]; 
+        // for(int i = 1 ; i < n ; i++){
+        //     prefMax[i] = Math.max(prefMax[i-1] , arr[i]); 
+        // }
+        // suffMax[n-1] = arr[n-1]; 
+        // for(int i = n-2 ; i >= 0 ; i--){
+        //     suffMax[i] = Math.max(suffMax[i + 1] , arr[i]); 
+        // }
+        // int res = 0 ; 
+        // for(int i = 0 ; i < n ; i++){
+        //     res += Math.min(prefMax[i] , suffMax[i]) - arr[i]; 
+        // }
+        // return res; 
+        
+        int leftMax = 0 ; 
+        int rightMax = 0  ;
+        int left = 0 , right = n - 1 ; 
+        int res = 0 ; 
+        
         while(left < right){
             if(arr[left] > leftMax){
-                leftMax = Math.max(leftMax, arr[left]);
+                leftMax = Math.max(leftMax , arr[left]); 
             }
             if(arr[right] > rightMax){
-                rightMax = Math.max(rightMax, arr[right]); 
+                rightMax = Math.max(rightMax , arr[right]); 
             }
+            
             if(leftMax < rightMax){
-                ans += (leftMax - arr[left]); 
-                left ++ ; 
+                res += leftMax - arr[left]; 
+                left ++; 
             }
             else {
-                ans += (rightMax - arr[right]);
+                res += rightMax - arr[right]; 
                 right -- ; 
             }
         }
-        return ans ; 
+        return res; 
     }
 }
