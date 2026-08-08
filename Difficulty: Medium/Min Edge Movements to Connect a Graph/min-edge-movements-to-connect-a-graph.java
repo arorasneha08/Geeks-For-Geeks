4b1f0d0023d8619c010d5d51c4d01b95ffd5729a
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2026-08-08 12:56:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Min Edge Movements to Connect a Graph/min-edge-movements-to-connect-a-graph.java
@@ -0,0 +1,38 @@
+class Solution {
+    int minEdgesReq(int n, int[][] edges) {
+        int m = edges.length;
+        
+        if (m < n - 1) {
+            return -1;
+        }
+        
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        
+        int components = n;
+        
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            
+            int rootU = find(parent, u);
+            int rootV = find(parent, v);
+            
+            if (rootU != rootV) {
+                parent[rootU] = rootV;
+                components--;
+            }
+        }
+        
+        return components - 1;
+    }
+    
+    private int find(int[] parent, int i) {
+        if (parent[i] != i) {
+            parent[i] = find(parent, parent[i]);
+        }
+        return parent[i];
+    }
+}
