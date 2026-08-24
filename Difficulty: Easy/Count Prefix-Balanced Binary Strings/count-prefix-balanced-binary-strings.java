cbace1e0e184ef19c01736bfaffe0331e9d6c348
Sneha Arora <158125379+arorasneha08@users.noreply.github.com>
2026-08-24 11:42:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Prefix-Balanced Binary Strings/count-prefix-balanced-binary-strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int prefixStrings(int n) {
+        long dp[]=new long[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            int l=0,r=i-1;
+            while(l<=i-1){
+                dp[i]=(dp[i]+(dp[l]*dp[r]))%1000000007;
+                l++;
+                r--;
+            }
+        }
+        return (int)dp[n]%1000000007;
+    }
+}
